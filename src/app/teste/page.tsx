4867e568eae5f9d0c2f62b3320a52c018d4af591
Luis Henrique <luishenriquealbino@gmail.com>
2025-01-07 15:18:53 -0300

    WIP

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  // Add props here! 
+}
+
+const page : React.FC<Props> = ({}) => {
+    return (
+      <>
+        Alguma coisa sobre alguma coisa
+      </>
+    );
+};
+
+export default page;
