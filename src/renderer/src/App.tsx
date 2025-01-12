badfeec798a0c7bfec6515870fa8293b92ef99c1
Luis Henrique <luishenriquealbino@gmail.com>
2025-01-12 10:12:06 -0300

    Config font family

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,20 +3,24 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import CustomDrawer from './components/CustomDrawer';
 import routes from './constants/routes';
+import { ThemeProvider } from '@mui/material';
+import theme from './libs/theme';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <CustomDrawer>
-        <Routes>
-          { 
-            routes.map((route) => (
-              <Route path={route.location} element={route.page} />    
-            ))
-          }
-        </Routes>
-      </CustomDrawer>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <CustomDrawer>
+          <Routes>
+            { 
+              routes.map((route) => (
+                <Route path={route.location} element={route.page} />    
+              ))
+            }
+          </Routes>
+        </CustomDrawer>
+      </Router>
+    </ThemeProvider>
   );
 };
 
---
+++ src/renderer/src/libs/theme.ts
@@ -0,0 +1,7 @@
+import { createTheme } from "@mui/material/styles";
+
+export default createTheme({
+  typography: {
+    fontFamily: `var(--font-family), 'Roboto', 'Arial', sans-serif`,
+  },
+});
